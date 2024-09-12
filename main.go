644c1daac2155a553055f8a8f7a48c2b76eb2bce
Dominik Froehlich <dominik.froehlich@sap.com>
2024-09-12 10:43:27 +0200

    use POD_IP instead

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"net/http"
 	"os"
 )
 
 func main() {
 
-	ip4 := getEgressv4IP()
-	ip6 := getEgressv6IP()
+	ip, ok := os.LookupEnv("POD_IP")
+	if !ok {
+		ip = "<POD_IP undefined>"
+	}
 
 	port, ok := os.LookupEnv("PORT")
 	if !ok {
@@ -19,7 +20,7 @@ func main() {
 
 	message, ok := os.LookupEnv("MESSAGE")
 	if !ok {
-		message = fmt.Sprintf("Hello World!\nfrom:\n%s (v4)\n%s (v6)\n", ip4, ip6)
+		message = fmt.Sprintf("Hello World!\nfrom: %s\n", ip)
 	}
 
 	mux := http.NewServeMux()
@@ -29,24 +30,4 @@ func main() {
 	})
 
 	_ = http.ListenAndServe(fmt.Sprintf(":%s", port), mux)
-}
-
-func getEgressv4IP() string {
-	conn, err := net.Dial("udp", "1.2.3.4:123")
-	if err != nil {
-		return err.Error()
-	}
-	defer conn.Close()
-
-	return conn.LocalAddr().(*net.UDPAddr).IP.String()
-}
-
-func getEgressv6IP() string {
-	conn, err := net.Dial("udp6", "[1:2:3:4:5:6:7:8]:123")
-	if err != nil {
-		return err.Error()
-	}
-	defer conn.Close()
-
-	return conn.LocalAddr().(*net.UDPAddr).IP.String()
 }
